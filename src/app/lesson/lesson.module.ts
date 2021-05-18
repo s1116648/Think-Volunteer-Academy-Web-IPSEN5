1a3bf68a2a237bf9fd4ea6add1559e18d1fd0675
Shifu-py <shivibadal@outlook.com>
2021-05-18 13:37:09 +0200

    Made progress circles automated

--- src/app/lesson/lesson.module.ts
+++ src/app/lesson/lesson.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 import { LessonOverviewComponent } from "./lesson-overview/lesson-overview.component";
 import { LessonOverviewCardComponent } from "./lesson-overview/lesson-overview-card/lesson-overview-card.component";
 import {LessonService} from "./lesson.service";
+import { ProgressCircleComponent } from "./lesson-overview/progress-circle/progress-circle.component";
 
 
 
 @NgModule({
-  declarations: [LessonOverviewComponent, LessonOverviewCardComponent],
+  declarations: [LessonOverviewComponent, LessonOverviewCardComponent, ProgressCircleComponent],
   imports: [
     CommonModule
   ],
