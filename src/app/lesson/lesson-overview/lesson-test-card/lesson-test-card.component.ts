f581aa88ac6031243ca6a0aab3ef35e83692a16b
Shifu-py <shivibadal@outlook.com>
2021-05-19 10:44:45 +0200

    Added test card + images

---
+++ src/app/lesson/lesson-overview/lesson-test-card/lesson-test-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-lesson-test-card",
+  templateUrl: "./lesson-test-card.component.html",
+  styleUrls: ["./lesson-test-card.component.scss"]
+})
+export class LessonTestCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/lesson/lesson.module.ts
+++ src/app/lesson/lesson.module.ts
@@ -5,11 +5,13 @@ import { LessonOverviewCardComponent } from "./lesson-overview/lesson-overview-c
 import {LessonService} from "./lesson.service";
 import { ProgressCircleComponent } from "./lesson-overview/progress-circle/progress-circle.component";
 import { RecommendationCardComponent } from "./lesson-overview/recommendation-card/recommendation-card.component";
+import { LessonTestCardComponent } from "./lesson-overview/lesson-test-card/lesson-test-card.component";
 
 
 
 @NgModule({
-  declarations: [LessonOverviewComponent, LessonOverviewCardComponent, ProgressCircleComponent, RecommendationCardComponent],
+  declarations: [LessonOverviewComponent, LessonOverviewCardComponent, ProgressCircleComponent,
+    RecommendationCardComponent, LessonTestCardComponent],
   imports: [
     CommonModule
   ],
