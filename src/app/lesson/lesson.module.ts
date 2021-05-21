fafce9bcc8d39d0a66e20bbe5a5da22bca87ca64
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-05-21 14:13:06 +0200

    routing module in lesson module

--- src/app/lesson/lesson.module.ts
+++ src/app/lesson/lesson.module.ts
@@ -2,26 +2,23 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LessonOverviewComponent } from "./lesson-overview/lesson-overview.component";
 import { LessonOverviewCardComponent } from "./lesson-overview/lesson-overview-card/lesson-overview-card.component";
-import {LessonService} from "./lesson.service";
+import { LessonService } from "./lesson.service";
 import { ProgressCircleComponent } from "./lesson-overview/progress-circle/progress-circle.component";
 import { RecommendationCardComponent } from "./lesson-overview/recommendation-card/recommendation-card.component";
 import { LessonTestCardComponent } from "./lesson-overview/lesson-test-card/lesson-test-card.component";
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-
-
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { LessonRoutingModule } from "./lesson-routing.module";
 
 @NgModule({
-  declarations: [LessonOverviewComponent, LessonOverviewCardComponent, ProgressCircleComponent,
-    RecommendationCardComponent, LessonTestCardComponent],
-  imports: [
-    CommonModule,
-    FontAwesomeModule
-  ],
-  exports: [
-    LessonOverviewComponent
-  ],
-  providers: [
-    LessonService
-  ]
+	declarations: [
+		LessonOverviewComponent,
+		LessonOverviewCardComponent,
+		ProgressCircleComponent,
+		RecommendationCardComponent,
+		LessonTestCardComponent,
+	],
+	imports: [CommonModule, FontAwesomeModule, LessonRoutingModule],
+	exports: [LessonOverviewComponent],
+	providers: [LessonService],
 })
-export class LessonModule { }
+export class LessonModule {}
