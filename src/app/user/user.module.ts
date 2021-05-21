1a34eaeb8cac8fca2ff5c73d16de1bd91ad69df7
Noah Slik <noahslik99@gmail.com>
2021-05-21 02:05:28 +0200

    Beginning of profile settings screen and profile cards. Everything is currently hardcoded and images and text are placeholders. Not linked with back-end yet.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserSettingsComponent, ProfileCardComponent],
+  exports: [
+    UserSettingsComponent
+  ],
   imports: [
     CommonModule
   ]
