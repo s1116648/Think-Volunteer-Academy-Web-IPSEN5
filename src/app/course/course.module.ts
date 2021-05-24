64aad45ffa9e8dad012e30cb678bc2d9312bb605
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-05-24 10:51:05 +0200

    create course component

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CourseRoutingModule } from "./course.routing.module";
+import { CreateCourseComponent } from "./create-course/create-course.component";
 
 @NgModule({
-	declarations: [],
+	declarations: [CreateCourseComponent],
 	imports: [CommonModule, CourseRoutingModule],
 })
 export class CourseModule {}
--- src/app/course/course.routing.module.ts
+++ src/app/course/course.routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { CreateCourseComponent } from "./create-course/create-course.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: "courses/new",
+		component: CreateCourseComponent,
+	},
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
