c947aa1dd2ea723a3bf426b690c2f60170c17c77
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-05-25 15:08:21 +0200

    general info on admin course page

--- src/app/lesson/lesson.service.ts
+++ src/app/lesson/lesson.service.ts
@@ -8,81 +8,11 @@ import { Lesson } from "./lesson.model";
 	providedIn: "root",
 })
 export class LessonService {
-	private lessons: Lesson[] = [
-		// Dummy data
-		{
-			id: "1",
-			name: "Culture of Bali",
-			image: "/assets/images/nature.jpeg",
-			content: "test",
-			length: 20,
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-			updatedAt: new Date("2021-11-01"),
-			createdAt: new Date("2020-11-01"),
-		},
-		{
-			id: "2",
-			name: "Culture of Bali",
-			image: "/assets/images/nature.jpeg",
-			content: "test",
-			length: 20,
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			updatedAt: new Date("2021-11-01"),
-			createdAt: new Date("2020-11-01"),
-		},
-		{
-			id: "3",
-			name: "Culture of Bali",
-			image: "/assets/images/nature.jpeg",
-			content: "test",
-			length: 20,
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			updatedAt: new Date("2021-11-01"),
-			createdAt: new Date("2020-11-01"),
-		},
-		{
-			id: "4",
-			name: "Culture of Bali",
-			image: "/assets/images/nature.jpeg",
-			content: "test",
-			length: 20,
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			updatedAt: new Date("2021-11-01"),
-			createdAt: new Date("2020-11-01"),
-		},
-		{
-			id: "10",
-			name: "Culture of Bali",
-			image: "/assets/images/nature.jpeg",
-			content: "test",
-			length: 20,
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			updatedAt: new Date("2021-11-01"),
-			createdAt: new Date("2020-11-01"),
-		},
-	];
-
 	constructor(private http: HttpClient) {}
 
 	get(courseID: string): Observable<HttpPaginatedResult<Lesson>> {
-		// return this.http.get<HttpPaginatedResult<Lesson>>(
-		// 	`/courses/${courseID}/lessons`
-		// );
-		return new Observable((o) =>
-			o.next({
-				pagination: {
-					currentPage: 0,
-					pageSize: 0,
-					totalItems: 0,
-					totalPages: 0,
-				},
-				items: this.lessons,
-			})
+		return this.http.get<HttpPaginatedResult<Lesson>>(
+			`/courses/${courseID}/lessons`
 		);
 	}
 }
