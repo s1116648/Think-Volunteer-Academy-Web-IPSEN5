fe4790ac4a5dd09a0c0256df6ddb82218db26baf
Noah Slik <noahslik99@gmail.com>
2021-05-26 17:03:32 +0200

    Fixed linter errors: user-routing.module.ts

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {UserSettingsComponent} from "./user-settings/user-settings.component";
 import {AuthGuard} from "../auth/auth.guard";
