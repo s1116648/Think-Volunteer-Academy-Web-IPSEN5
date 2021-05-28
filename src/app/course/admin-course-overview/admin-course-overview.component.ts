ebc1d059991f59fac666bc6d46881862cdf0fcf1
Lars Verhoorn <dankgaming2017@gmail.com>
2021-05-28 16:32:46 +0200

    UC28 Les aanmaken

--- src/app/course/admin-course-overview/admin-course-overview.component.ts
+++ src/app/course/admin-course-overview/admin-course-overview.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 import { Course } from "../course.model";
 import { CourseService } from "../course.service";
 import { faPen, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { SetRoleModalComponent } from "../../role/modals/set-role-modal/set-role-modal.component";
+import { ModalService } from "../../shared/modal.service";
+import { PlaceholderDirective } from "../../shared/placeholder.directive";
+import { SetLessonModalComponent } from "../../lesson/modals/set-lesson-modal/set-lesson-modal.component";
+import { Lesson } from "../../lesson/lesson.model";
 
 @Component({
 	selector: "app-admin-course-overview",
@@ -10,6 +15,9 @@ import { faPen, faPlus } from "@fortawesome/free-solid-svg-icons";
 	styleUrls: ["./admin-course-overview.component.scss"],
 })
 export class AdminCourseOverviewComponent implements OnInit {
+  @ViewChild(PlaceholderDirective, { static: false })
+  modalHost: PlaceholderDirective;
+
 	icons = { faPen, faPlus };
 
 	course: Course;
@@ -20,7 +28,10 @@ export class AdminCourseOverviewComponent implements OnInit {
 
 	constructor(
 		private courseService: CourseService,
-		private route: ActivatedRoute
+		private route: ActivatedRoute,
+  private modalService: ModalService,
+  private activatedRoute: ActivatedRoute,
+  private router: Router
 	) {}
 
 	ngOnInit(): void {
@@ -32,4 +43,17 @@ export class AdminCourseOverviewComponent implements OnInit {
 				});
 		});
 	}
+
+  openNewLessonModal(): void {
+    const modal = this.modalService.createModal(
+      SetLessonModalComponent,
+      this.modalHost
+    );
+    modal.instance.course = this.course;
+    modal.instance.set.subscribe((lesson: Lesson) => {
+      this.router.navigate(["lessons", lesson.id], {
+        relativeTo: this.route,
+      });
+    });
+  }
 }
--- src/app/lesson/lesson.module.ts
+++ src/app/lesson/lesson.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CourseService } from "../course/course.service";
 import { LessonRoutingModule } from "./lesson-routing.module";
+import { SetLessonModalComponent } from './modals/set-lesson-modal/set-lesson-modal.component';
+import { SharedModule } from "../shared/shared.module";
+import { FormsModule } from "@angular/forms";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-	declarations: [],
-	imports: [CommonModule, LessonRoutingModule],
+	declarations: [SetLessonModalComponent],
+  imports: [CommonModule, LessonRoutingModule, SharedModule, FormsModule, FontAwesomeModule],
 	exports: [],
 	providers: [CourseService],
 })
