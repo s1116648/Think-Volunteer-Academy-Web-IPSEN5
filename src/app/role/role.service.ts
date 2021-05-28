88f4a1f081aff44c9e219a749b90c8490e2c6836
Lars Verhoorn <dankgaming2017@gmail.com>
2021-05-28 13:04:54 +0200

    fixed mock data

--- src/app/role/role.service.ts
+++ src/app/role/role.service.ts
@@ -13,15 +13,21 @@ export class RoleService {
   roles: Role[] = [
     {
       id: 1,
-      name: "admin"
+      name: "admin",
+      createdAt: new Date(),
+      updatedAt: new Date()
     },
     {
       id: 2,
-      name: "contentcreator"
+      name: "contentcreator",
+      createdAt: new Date(),
+      updatedAt: new Date()
     },
     {
       id: 3,
-      name: "user"
+      name: "user",
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
   ];
 
