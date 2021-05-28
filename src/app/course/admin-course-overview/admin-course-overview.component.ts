24d5d27032e2ca3b3038f5d1bf137fb4d0acf244
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-05-28 14:50:09 +0200

    admin lesson cards

--- src/app/course/admin-course-overview/admin-course-overview.component.ts
+++ src/app/course/admin-course-overview/admin-course-overview.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute, Params } from "@angular/router";
 import { Course } from "../course.model";
 import { CourseService } from "../course.service";
 import { faPen, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Lesson } from "src/app/lesson/lesson.model";
+import { LessonService } from "src/app/lesson/lesson.service";
+import { HttpPaginatedResult } from "src/app/shared/http-paginated-result";
 
 @Component({
 	selector: "app-admin-course-overview",
@@ -13,6 +16,7 @@ export class AdminCourseOverviewComponent implements OnInit {
 	icons = { faPen, faPlus };
 
 	course: Course;
+	lessons: Lesson[];
 
 	get totalLessonLength(): number {
 		return 0;
@@ -20,6 +24,7 @@ export class AdminCourseOverviewComponent implements OnInit {
 
 	constructor(
 		private courseService: CourseService,
+		private lessonService: LessonService,
 		private route: ActivatedRoute
 	) {}
 
@@ -29,6 +34,12 @@ export class AdminCourseOverviewComponent implements OnInit {
 				.getByID(params.id)
 				.subscribe((course: Course) => {
 					this.course = course;
+				});
+
+			this.lessonService
+				.get(params.id)
+				.subscribe((paginated: HttpPaginatedResult<Lesson>) => {
+					this.lessons = paginated.items;
 				});
 		});
 	}
