e253c0947ffbc18a8a1d44a80fe346d002864450
Shifu-py <shivibadal@outlook.com>
2021-06-02 11:49:43 +0200

    Formatted with prettier

--- src/app/lesson/lesson-view/lesson-documents-card/lesson-documents-card.component.ts
+++ src/app/lesson/lesson-view/lesson-documents-card/lesson-documents-card.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-lesson-documents-card",
-  templateUrl: "./lesson-documents-card.component.html",
-  styleUrls: ["./lesson-documents-card.component.scss"]
+	selector: "app-lesson-documents-card",
+	templateUrl: "./lesson-documents-card.component.html",
+	styleUrls: ["./lesson-documents-card.component.scss"],
 })
 export class LessonDocumentsCardComponent implements OnInit {
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- src/app/lesson/lesson-view/lesson-view.component.ts
+++ src/app/lesson/lesson-view/lesson-view.component.ts
@@ -1,38 +1,40 @@
 import { Component, OnInit } from "@angular/core";
-import {Course} from "../../course/course.model";
-import {ActivatedRoute, Params} from "@angular/router";
-import {LessonService} from "../lesson.service";
-import {Lesson} from "../lesson.model";
-import {CourseService} from "../../course/course.service";
+import { Course } from "../../course/course.model";
+import { ActivatedRoute, Params } from "@angular/router";
+import { LessonService } from "../lesson.service";
+import { Lesson } from "../lesson.model";
+import { CourseService } from "../../course/course.service";
 
 @Component({
-  selector: "app-lesson-view",
-  templateUrl: "./lesson-view.component.html",
-  styleUrls: ["./lesson-view.component.scss"]
+	selector: "app-lesson-view",
+	templateUrl: "./lesson-view.component.html",
+	styleUrls: ["./lesson-view.component.scss"],
 })
 export class LessonViewComponent implements OnInit {
-  lesson: Lesson;
-  course: Course;
-  constructor(private route: ActivatedRoute,
-              private lessonService: LessonService,
-              private courseService: CourseService) { }
+	lesson: Lesson;
+	course: Course;
+	constructor(
+		private route: ActivatedRoute,
+		private lessonService: LessonService,
+		private courseService: CourseService
+	) {}
 
-  ngOnInit(): void {
-      this.route.params.subscribe((params: Params) => {
-          this.lessonService
-              .getById(params.id)
-              .subscribe((lesson: Lesson) => {
-                  this.lesson = lesson;
-                  this.getCourseFromId();
-              });
-      });
-  }
+	ngOnInit(): void {
+		this.route.params.subscribe((params: Params) => {
+			this.lessonService
+				.getById(params.id)
+				.subscribe((lesson: Lesson) => {
+					this.lesson = lesson;
+					this.getCourseFromId();
+				});
+		});
+	}
 
-  getCourseFromId(): void{
-      this.courseService
-          .getByID(this.lesson.courseId)
-          .subscribe((course: Course) => {
-              this.course = course;
-          });
-  }
+	getCourseFromId(): void {
+		this.courseService
+			.getByID(this.lesson.courseId)
+			.subscribe((course: Course) => {
+				this.course = course;
+			});
+	}
 }
