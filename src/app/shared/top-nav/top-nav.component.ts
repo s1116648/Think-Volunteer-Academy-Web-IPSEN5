0b07ab32894138ec316b8a8dde3a3a4415526b6f
Carl <carlzee@live.nl>
2021-06-03 13:53:22 +0200

    Navbar met werkende logout, wel 2 todo's voor als de routing daarvoor werkt. En nog een todo voor als permissions werken.

--- src/app/shared/top-nav/top-nav.component.ts
+++ src/app/shared/top-nav/top-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../auth/auth.service";
 
 @Component({
   selector: "app-top-nav",
@@ -10,19 +11,28 @@ export class TopNavComponent implements OnInit {
     coursesPath = "/courses";
     adminPanelPath = ""; // ToDo insert admin path
     settingsPath = ""; // ToDo insert settings path
-    logoutPath = ""; // ToDo insert logout path
+    logoutPath = "/login";
 
     isAdmin: boolean;
 
-    constructor() {}
+    constructor(private authService: AuthService) {}
 
     ngOnInit(): void {
         this.isAdmin = this.checkIfAdmin();
     }
 
+    // ToDo, don't create it if you aren't logged in.
+    checkIfLoggedIn(): boolean {
+        return false;
+    }
+
     // ToDo If permissions are finished, make this method useful.
     checkIfAdmin(): boolean {
         return true;
+    }
+
+    logout(): void {
+        this.authService.logout();
     }
 
 }
