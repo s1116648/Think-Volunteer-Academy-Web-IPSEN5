332ea266984712c21856b479deddb8777141d3eb
Carl <carlzee@live.nl>
2021-06-04 13:50:47 +0200

    single quote mark naar double veranderd

--- src/app/shared/top-nav/top-nav.component.spec.ts
+++ src/app/shared/top-nav/top-nav.component.spec.ts
@@ -1,8 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { TopNavComponent } from './top-nav.component';
+import { TopNavComponent } from "./top-nav.component";
 
-describe('TopNavComponent', () => {
+describe("TopNavComponent", () => {
   let component: TopNavComponent;
   let fixture: ComponentFixture<TopNavComponent>;
 
@@ -19,7 +19,7 @@ describe('TopNavComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
