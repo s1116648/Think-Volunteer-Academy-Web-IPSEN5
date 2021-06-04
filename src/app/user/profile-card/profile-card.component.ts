e2e352e924b3a581d9828325c4356340fbacad24
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-06-04 15:58:31 +0200

    profile card css

--- src/app/user/profile-card/profile-card.component.ts
+++ src/app/user/profile-card/profile-card.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from "@angular/core";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-    selector: "app-profile-card",
-    templateUrl: "./profile-card.component.html",
-    styleUrls: ["./profile-card.component.scss"]
+	selector: "app-profile-card",
+	templateUrl: "./profile-card.component.html",
+	styleUrls: ["./profile-card.component.scss"],
 })
 export class ProfileCardComponent implements OnInit {
+	icons = { faArrowRight };
 
-    firstname: string;
-    lastname: string;
+	firstname: string;
+	lastname: string;
 
-    constructor() {
-    }
+	constructor() {}
 
-    ngOnInit(): void {
-        const currentUser = JSON.parse(localStorage.getItem("loginInfo")).user;
-        this.setName(currentUser.firstname, currentUser.lastname);
-    }
+	ngOnInit(): void {
+		const currentUser = JSON.parse(localStorage.getItem("loginInfo")).user;
+		this.setName(currentUser.firstname, currentUser.lastname);
+	}
 
-    setName(firstname: string, lastname: string): void {
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
+	setName(firstname: string, lastname: string): void {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
 }
