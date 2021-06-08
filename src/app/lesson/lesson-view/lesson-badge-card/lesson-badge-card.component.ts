da637f9931cbb0f18255928c3953bbaa647f6786
Shifu-py <shivibadal@outlook.com>
2021-06-08 12:57:58 +0200

    Badges can now be seen within a lesson view and i rewrote roman numerals method

--- src/app/lesson/lesson-view/lesson-badge-card/lesson-badge-card.component.ts
+++ src/app/lesson/lesson-view/lesson-badge-card/lesson-badge-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import {Badge} from "../../../shared/badge.model";
+import {Lesson} from "../../lesson.model";
 
 @Component({
 	selector: "app-lesson-badge-card",
@@ -6,7 +8,16 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./lesson-badge-card.component.scss"],
 })
 export class LessonBadgeCardComponent implements OnInit {
+	@Input() lesson: Lesson;
+	@Input() badges: Badge[];
+
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+
+	}
+	lessonIsCompleted(lessonNumber: number): boolean{
+		return this.badges.map(badge => badge.lessonNumber).includes(lessonNumber);
+	}
+
 }
