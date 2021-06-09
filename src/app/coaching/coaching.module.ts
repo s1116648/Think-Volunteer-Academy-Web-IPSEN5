54ba3fcf68fcc6c307a8b23bd1ac83edafc85042
Lars Verhoorn <dankgaming2017@gmail.com>
2021-06-09 21:49:06 +0200

    no message

--- src/app/coaching/coaching.module.ts
+++ src/app/coaching/coaching.module.ts
@@ -4,8 +4,8 @@ import { CoachOverviewComponent } from "./coach/coach-overview/coach-overview.co
 import { StudentOverviewComponent } from "./student/student-overview/student-overview.component";
 import { CoachingRoutingModule } from "./coaching-routing.module";
 import { SharedModule } from "../shared/shared.module";
-import { AddStudentModalComponent } from './coach/modals/add-student-modal/add-student-modal.component';
-import { AddCoachModalComponent } from './coach/modals/add-coach-modal/add-coach-modal.component';
+import { AddStudentModalComponent } from "./coach/modals/add-student-modal/add-student-modal.component";
+import { AddCoachModalComponent } from "./coach/modals/add-coach-modal/add-coach-modal.component";
 import { FormsModule } from "@angular/forms";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
