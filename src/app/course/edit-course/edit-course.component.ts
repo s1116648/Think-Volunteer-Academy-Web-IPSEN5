a681fe315788b1b2954367f817b9d8d5455217f2
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-06-10 15:10:35 +0200

    small fixes

--- src/app/course/edit-course/edit-course.component.ts
+++ src/app/course/edit-course/edit-course.component.ts
@@ -78,7 +78,7 @@ export class EditCourseComponent implements OnInit {
 	remove(): void {
 		this.courseService
 			.remove(this.course.id)
-			.subscribe(() => this.router.navigate(["/admin", "courses"]));
+			.subscribe(() => this.router.navigate(["/admin"]));
 	}
 
 	showCreateCategoryModal(): void {
