0997896b18898213c5bf573039761788d2be11cc
Shifu-py <shivibadal@outlook.com>
2021-06-11 13:56:42 +0200

    Made general layout for edit test view

--- src/app/test/admin-edit-test-view/admin-edit-test-view.component.ts
+++ src/app/test/admin-edit-test-view/admin-edit-test-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import {Lesson} from "../../lesson/lesson.model";
+import {LessonService} from "../../lesson/lesson.service";
+import {ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: "app-admin-edit-test-view",
@@ -6,10 +9,19 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./admin-edit-test-view.component.scss"]
 })
 export class AdminEditTestViewComponent implements OnInit {
+  lesson: Lesson;
 
-  constructor() { }
+  constructor(private lessonService: LessonService,
+              private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+      this.route.params.subscribe((params: Params) => {
+          this.lessonService
+              .getById(params.lessonId)
+              .subscribe((lesson: Lesson) => {
+                  this.lesson = lesson;
+              });
+      });
   }
 
 }
