5e68d8b77afbbdc2e85f6114884a1867a0c95ac4
Carl <carlzee@live.nl>
2021-06-22 14:44:29 +0200

    Next lesson werkt

--- src/app/lesson/lesson-view/lesson-view.component.ts
+++ src/app/lesson/lesson-view/lesson-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { Course } from "../../course/course.model";
 import { ActivatedRoute, Params, Router } from "@angular/router";
 import { LessonService } from "../lesson.service";
@@ -8,74 +8,99 @@ import { HttpPaginatedResult } from "../../shared/http-paginated-result";
 import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 import { LessonAttachment } from "src/app/lesson-attachment/lesson-attachment.model";
 import { LessonAttachmentService } from "src/app/lesson-attachment/lesson-attachment.service";
-import {BadgeService} from "../../shared/badge.service";
-import {AuthService} from "../../auth/auth.service";
-import {Badge} from "../../shared/badge.model";
+import { BadgeService } from "../../shared/badge.service";
+import { AuthService } from "../../auth/auth.service";
+import { Badge } from "../../shared/badge.model";
 import { faGraduationCap, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-	selector: "app-lesson-view",
-	templateUrl: "./lesson-view.component.html",
-	styleUrls: ["./lesson-view.component.scss"],
+    selector: "app-lesson-view",
+    templateUrl: "./lesson-view.component.html",
+    styleUrls: ["./lesson-view.component.scss"]
 })
 export class LessonViewComponent implements OnInit {
-	icons = { faGraduationCap, faChevronRight };
-	lesson: Lesson;
-	course: Course;
-	badges: Badge[];
-	index: number;
-	attachments: LessonAttachment[] = [];
 
-	constructor(
-		private route: ActivatedRoute,
-		private lessonService: LessonService,
-		private courseService: CourseService,
-		private lessonAttachmentService: LessonAttachmentService,
-		private router: Router,
-		private sanitizer: DomSanitizer,
-		private badgeService: BadgeService,
-		private authService: AuthService
-	) {}
+    icons = { faGraduationCap, faChevronRight };
+    lesson: Lesson;
+    course: Course;
+    badges: Badge[];
+    attachments: LessonAttachment[] = [];
+    nextLesson: Lesson;
+    nextLessonExists: boolean;
 
-	ngOnInit(): void {
-		this.route.params.subscribe((params: Params) => {
-			this.lessonService
-				.getById(params.lessonId)
-				.subscribe((lesson: Lesson) => {
-					this.lesson = lesson;
-					this.getCourseFromId();
-					this.getAttachmentsByLessonId();
-					this.getBadgesFromCourseId();
-				});
-		});
-	}
+    constructor(
+        private route: ActivatedRoute,
+        private lessonService: LessonService,
+        private courseService: CourseService,
+        private lessonAttachmentService: LessonAttachmentService,
+        private router: Router,
+        private sanitizer: DomSanitizer,
+        private badgeService: BadgeService,
+        private authService: AuthService
+    ) {
+    }
 
-	getSafeHTML = (): SafeHtml =>
-		this.sanitizer.bypassSecurityTrustHtml(this.lesson.content);
+    ngOnInit(): void {
+        this.route.params.subscribe((params: Params) => {
+            this.lessonService
+                .getById(params.lessonId)
+                .subscribe((lesson: Lesson) => {
+                    this.lesson = lesson;
+                    this.getCourseFromId();
+                    this.getAttachmentsByLessonId();
+                    this.getBadgesFromCourseId();
+                });
+        });
+    }
 
-	getAttachmentsByLessonId(): void {
-		this.lessonAttachmentService
-			.get(this.lesson.id)
-			.subscribe(
-				(responseData: HttpPaginatedResult<LessonAttachment>) => {
-					this.attachments = responseData.items;
-				}
-			);
-	}
+    getSafeHTML = (): SafeHtml =>
+        this.sanitizer.bypassSecurityTrustHtml(this.lesson.content);
 
-	getCourseFromId(): void {
-		this.courseService
-			.getByID(this.lesson.courseId)
-			.subscribe((course: Course) => {
-				this.course = course;
-			});
-	}
+    getAttachmentsByLessonId(): void {
+        this.lessonAttachmentService
+            .get(this.lesson.id)
+            .subscribe(
+                (responseData: HttpPaginatedResult<LessonAttachment>) => {
+                    this.attachments = responseData.items;
+                }
+            );
+    }
 
-	getBadgesFromCourseId(): void {
-		this.badgeService.getBadgesByUser(this.authService.loginInfo.getValue().user.id, this.lesson.courseId)
-			.subscribe((result: HttpPaginatedResult<Badge>) => {
-				this.badges = result.items;
-			});
-	}
+    getCourseFromId(): void {
+        this.courseService
+            .getByID(this.lesson.courseId)
+            .subscribe((course: Course) => {
+                this.course = course;
+                this.setsNextLesson();
+            });
+    }
 
+    getBadgesFromCourseId(): void {
+        this.badgeService.getBadgesByUser(this.authService.loginInfo.getValue().user.id, this.lesson.courseId)
+            .subscribe((result: HttpPaginatedResult<Badge>) => {
+                this.badges = result.items;
+            });
+    }
+
+    setsNextLesson(): void {
+        this.lessonService.get(this.course.id).subscribe((value) => {
+            const lessons = value.items;
+            let foundCurrentLesson = false;
+            for (let i = 0; i < lessons.length; i++) {
+                if (foundCurrentLesson) {
+                    this.nextLesson = lessons[i];
+                    this.nextLessonExists = true;
+                    console.log("next lesson exists!");
+                    return;
+                }
+                if (this.lesson.id === lessons[i].id) {
+                    foundCurrentLesson = true;
+                }
+            }
+            this.nextLessonExists = false;
+        });
+    }
+
+    getSafeHTMLNextLesson = (): SafeHtml =>
+        this.sanitizer.bypassSecurityTrustHtml(this.nextLesson.content);
 }
